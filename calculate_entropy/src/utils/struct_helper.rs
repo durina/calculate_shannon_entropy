d787e7890db8314ba7b6fffa99221a68e8a635d4
durina <knanirudh@gmail.com>
2023-09-19 17:49:48 +0530

    Introduced file helper to reduce redundancy in creating the file buffer.

---
+++ calculate_entropy/src/utils/struct_helper.rs
@@ -0,0 +1,27 @@
+
+// struct to handle file buffers
+
+
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
+use log::{error, info};
+
+pub struct FileBufferHelper<'a>{
+    pub(crate) path: &'a PathBuf,
+    pub buffer_reader: BufReader<&'a File>,
+    pub line: String
+}
+
+impl FileBufferHelper {
+    pub fn new(file: &PathBuf) -> FileBufferHelper {
+        let mut line = String::new();
+        let file_open = File::open(&file).unwrap();
+        let mut read_file = BufReader::new(&file_open);
+        Self {
+            path: &file,
+            buffer_reader: read_file,
+            line,
+        }
+    }
+}

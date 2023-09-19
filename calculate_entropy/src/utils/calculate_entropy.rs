699db4077dffc9b41a6063ad86741637f7693fa9
durina <knanirudh@gmail.com>
2023-09-19 17:50:29 +0530

    Refactored to employ the file helper struct

--- calculate_entropy/src/utils/calculate_entropy.rs
+++ calculate_entropy/src/utils/calculate_entropy.rs
@@ -15,62 +15,66 @@ use std::{io::{BufRead, BufReader, Seek, SeekFrom, BufWriter, Write}, sync::{Mut
 use super::get_args::Mode;
 use threadpool::ThreadPool;
 use log::{debug, error, warn, info};
+use crate::utils::struct_helper::FileBufferHelper;
 
 const STANDARD_DNA_NOTATIONS: &str = "ATGC";
 const ALL_DNA_NOTATIONS: &str = "ATGCUWSMKRYBDHVN";
 const DNA_ALIGNMENT_NOTATIONS: &str = "-.";
 
-pub fn report_entropy(file: &PathBuf, mode: Mode, threshold: f64, nproc: usize, suffix: &String) {
+pub fn report_entropy(file: &mut FileBufferHelper, mode: Mode,
+                      threshold: f64, nproc: usize, suffix: &String) {
     // open validated alignment file from main()
-    let alignment_file = File::open(&file).unwrap();
-    // establish file buffer to read contents of the alignment
-    let mut read_alignment_file = BufReader::new(&alignment_file);
-    debug!("Opened the file buffer");
     // initialise the HashMap of DNA notatations for every position of the alignment
-    let count_vec: Vec<HashMap<char, u32>> = initialise_structs(&file, &mode,
-                                                                &mut read_alignment_file);
+    let count_vec: Vec<HashMap<char, u32>> = initialise_structs(file, &mode);
     info!("Positions initialised");
-    read_alignment_file.seek(SeekFrom::Start(0)).unwrap();
+    // reset file buffer to zero
+    file.buffer_reader.seek(SeekFrom::Start(0)).unwrap();
     debug!("Reset file buffer position to start");
-    process_genomes(count_vec, threshold, mode, nproc,
-                    &mut read_alignment_file, suffix, &file);
+    // count the occurrence of respective notations at every position
+    // count_vec: Position wise count of DNA notations from the alignment
+    // threshold: Fraction of positions needed to be filled across a
+    //              position in the alignment for the position to be considered
+    // mode: Consider only Standard, or All accepted IUPAC notations
+    // nproc: # number of processors to be involved
+    // read_alignment_file: file buffer mapped to the alignment file
+    // suffix: suffix to be added while saving the final output file
+    // file: location of the alignment file
+    process_genomes(count_vec, threshold, mode, nproc, file, suffix);
 }
 
 // Initialise the each position in the alignment
 
-fn initialise_structs(file: &PathBuf, mode: &Mode, buffer: &mut BufReader<&File>)
-                                                                    -> Vec<HashMap<char, u32>> {
+fn initialise_structs(file: &mut FileBufferHelper, mode: &Mode) -> Vec<HashMap<char, u32>> {
     // reading into the first genome
     // estimate length of the alignment
     // initialise Vec of HashMaps
     debug!("Preparing the columns for analysis");
-    let mut line = String::new();
     let mut temp_genome = String::new();
     let mut temp_genome_length = 0usize;
     let mut flag: bool = false;
     // get one sample genome sequence
-    info!("Assessing length of alignments in {:?}", file);
+    info!("Assessing length of alignments in {:?}", file.path);
     // read the sequence from file buffer
     // Assumptions:
     // The alignment starts with the header of a particular sequence denoted by ">"
     // The sequence follows the header on the following lines
     // The next sequence is either interrupted by a newline or the next header
-    while buffer.read_line(&mut line).unwrap_or(0) > 1 {
-        if line.starts_with('\n') {
+    while file.buffer_reader.read_line(&mut file.line).unwrap_or(0) > 1 {
+        if file.line.starts_with('\n') {
             flag = true
-        } else if line.starts_with('>') && flag {
+        } else if file.line.starts_with('>') && flag {
             info!("While loop terminated");
-            line.clear();
+            file.line.clear();
             break
-        } else if line.starts_with('>') {
+        } else if file.line.starts_with('>') {
             debug!("{}", line.trim());
             flag = true;
         } else {
-            temp_genome_length += line.trim().len();
-            temp_genome = temp_genome + &line.trim();
+            temp_genome_length += file.line.trim().len();
+            temp_genome = temp_genome + &file.line.trim();
             flag = true;
         }
-        line.clear()
+        file.line.clear()
     }
     // crosscheck estimate of length of genome
     if temp_genome.len() != temp_genome_length {
@@ -101,19 +105,22 @@ fn initialise_structs(file: &PathBuf, mode: &Mode, buffer: &mut BufReader<&File>
 
 // Tabulate the frequency of each notation at the given position
 
-fn process_genomes(count_vec: Vec<HashMap<char, u32>>, threshold: f64, mode: Mode, nproc: usize, buffer: &mut BufReader<&File>, suffix: &String, file: &PathBuf) {
-    let mut line = String::new();
+fn process_genomes(count_vec: Vec<HashMap<char, u32>>, threshold: f64,
+                   mode: Mode, nproc: usize, file: &mut FileBufferHelper,
+                   suffix: &String) {
     let mut temp_genome = String::new();
     let mut header = String::new();
-    let arc_count_vec: Arc<Mutex<Vec<HashMap<char, u32>>>> = Arc::new(Mutex::new(count_vec));
+    let arc_count_vec: Arc<Mutex<Vec<HashMap<char, u32>>>> = Arc::new(
+                                                        Mutex::new(
+                                                            count_vec));
     let pool = ThreadPool::new(nproc);
     let mut genome_count: u32 = 0u32;
     // analyse all genomes
-    while buffer.read_line(&mut line).unwrap_or(0) > 1 {
-        if line.starts_with('>') {
+    while file.buffer_reader.read_line(&mut file.line).unwrap_or(0) > 1 {
+        if file.line.starts_with('>') {
             debug!("Analysing {}", line);
             if header.is_empty() && temp_genome.is_empty() {
-                header =  line.trim().to_string();
+                header =  file.line.trim().to_string();
             } else if !temp_genome.is_empty() {
                 debug!("Processing {}", header);
                 let arc_clone = Arc::clone(&arc_count_vec);
@@ -122,17 +129,17 @@ fn process_genomes(count_vec: Vec<HashMap<char, u32>>, threshold: f64, mode: Mod
                 pool.execute(move || {
                     analyse_genomes(temp_string, arc_clone, temp_header);
                 });
-                header =  line.trim().to_string();
+                header =  file.line.trim().to_string();
             }
             genome_count += 1;
-        } else if line.starts_with('\n') {
+        } else if file.line.starts_with('\n') {
             debug!("Empty line");
-            line.clear();
+            file.line.clear();
             continue;
         } else {
-            temp_genome = temp_genome + &line.trim()
+            temp_genome = temp_genome + &file.line.trim()
         }
-        line.clear();
+        file.line.clear();
     }
     pool.join();
     info!("Threadpool jobs complete");
@@ -143,7 +150,8 @@ fn process_genomes(count_vec: Vec<HashMap<char, u32>>, threshold: f64, mode: Mod
 
 // Handle multiple thread requests, call update_counts
 
-fn analyse_genomes(genome: String, arc_clone: Arc<Mutex<Vec<HashMap<char, u32>>>>, header: String) {
+fn analyse_genomes(genome: String, arc_clone: Arc<Mutex<Vec<HashMap<char, u32>>>>,
+                                                                        header: String) {
     if let Ok(mut count_vec) = arc_clone.lock() {
         genome.chars().enumerate().for_each( |(idx, x)| {
                 update_counts(&mut count_vec[idx], &x, &idx, &header)
@@ -153,7 +161,8 @@ fn analyse_genomes(genome: String, arc_clone: Arc<Mutex<Vec<HashMap<char, u32>>>
 }
 
 // update counts at given location
-fn update_counts(column: &mut HashMap<char, u32>, letter: &char, position: &usize, header: &String) {
+fn update_counts(column: &mut HashMap<char, u32>, letter: &char, position: &usize,
+                                                                        header: &String) {
     if let Some(val) = column.get_mut(&letter.to_ascii_uppercase()) {
         *val += 1
     } else {
@@ -166,7 +175,8 @@ fn update_counts(column: &mut HashMap<char, u32>, letter: &char, position: &usiz
 
 // arrive at Shannon entropy at each position
 
-fn finalise_counts(map_vec: Vec<HashMap<char, u32>>, genome_count: u32, threshold: f64, mode: Mode, suffix: &String, file: &PathBuf) {
+fn finalise_counts(map_vec: Vec<HashMap<char, u32>>, genome_count: u32,
+                   threshold: f64, mode: Mode, suffix: &String, file: &mut FileBufferHelper) {
     // calculate the shannon entropy at every position
     // shannon entropy = sum(-p log_2 p)
     let atgc: &str = if mode == Mode::Standard {
@@ -174,11 +184,15 @@ fn finalise_counts(map_vec: Vec<HashMap<char, u32>>, genome_count: u32, threshol
     } else {
         ALL_DNA_NOTATIONS
     };
-    info!("Output file: {}{suffix}", file.to_str().unwrap());
-    let mut entropy_writer = BufWriter::new(File::create(format!("{}{}", file.to_str().unwrap(), suffix)).unwrap());
+    info!("Output file: {}{suffix}", file.path.to_str().unwrap());
+    let mut entropy_writer = BufWriter::new(
+                File::create(format!("{}{}", file.path.to_str().unwrap(), suffix)
+                                                                                    ).unwrap());
     let mut atgc_count_vec: Vec<&u32> = Vec::with_capacity(atgc.len());
     info!("Notations considered to calculate Shannon entropy: {}",  atgc);
-    let count_headers = atgc.chars().fold(String::new(), |fin_str, x| fin_str + "\tCount_" + &x.to_string());
+    let count_headers = atgc.chars()
+                                    .fold(String::new(), |fin_str, x|
+                                                            fin_str + "\tCount_" + &x.to_string());
     let headers = format!("Position\
                         {count_headers}\t\
                         Genome_count\t\
@@ -193,13 +207,16 @@ fn finalise_counts(map_vec: Vec<HashMap<char, u32>>, genome_count: u32, threshol
             let atgc_share: f64 = atgc_count_vec.iter().map(|&&x| f64::from(x)).sum(); 
             let atgc_fraction = atgc_share/f64::from(genome_count);
             let entropy: f64 = get_entropy(&atgc_count_vec, &genome_count);
-            let n_counts = atgc_count_vec.drain(..).map(|x| format!("{x}\t")).collect::<String>();
+            let n_counts = atgc_count_vec.drain(..)
+                                        .map(|x| format!("{x}\t")).collect::<String>();
             let validity = if atgc_fraction >= threshold {
                 format!("Valid. Threshold = {}", threshold)
             } else {
                 format!("Invalid. Threshold = {}", threshold)
             };
-            writeln!(entropy_writer, "{pos}\t{counts}{genome_count}\t{atgc_share}\t{atgc_fraction}\t{shannon}\t{validity}", pos=idx+1, counts=n_counts,shannon=entropy).unwrap();
+            writeln!(entropy_writer, "{pos}\t{counts}{genome_count}\t{atgc_share}\t\
+                                                            {atgc_fraction}\t{shannon}\t{validity}",
+                                             pos=idx+1, counts=n_counts,shannon=entropy).unwrap();
         }
     );
 }
